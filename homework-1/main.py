2fd950e679e6cae9039c5f758e3c25d70cbe575c
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-07-19 19:23:42 +0300

    changed create_tables.sql and main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,35 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+
+def load_from_csv(path_csv: str, table_name: str):
+    with open(path_csv, 'r') as f:
+        csv_reader = csv.reader(f)
+
+        insert_query = f"INSERT INTO {table_name} VALUES ({','.join(['%s'] * len(next(csv_reader)))})"
+
+        for row in csv_reader:
+            cur.execute(insert_query, tuple(row))
+
+
+path_customers_csv = 'north_data/customers_data.csv'
+path_employees_csv = 'north_data/employees_data.csv'
+path_orders_csv = 'north_data/orders_data.csv'
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="b6gFXb"
+)
+try:
+    with conn:
+        with conn.cursor() as cur:
+
+            load_from_csv(path_customers_csv, "customers")
+            load_from_csv(path_employees_csv, "employees")
+            load_from_csv(path_orders_csv, "orders")
+
+finally:
+    conn.close()
